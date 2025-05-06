77b060b4fc8466f1ab293f5e8c983078a9af621e
ACT-STUDENT <ACT-STUDENT@LAB4-PC28>
2025-05-06 10:43:35 +0800

    Add project files.

---
+++ EVEDRI Github Exercise/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVEDRI_Github_Exercise
+{
+    public class Person
+    {
+
+    }
+}

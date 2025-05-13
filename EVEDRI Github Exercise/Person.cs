805d492a06e08a1731c1ce54ca52a6f984ebb5aa
ACT-STUDENT <ACT-STUDENT@LAB4-PC26>
2025-05-13 10:56:40 +0800

    mark christian canasa jr.

--- EVEDRI Github Exercise/Person.cs
+++ EVEDRI Github Exercise/Person.cs
@@ -8,6 +8,13 @@ namespace EVEDRI_Github_Exercise
 {
     public class Person
     {
+        public string Name { get; set; }    
+        public int Age { get; set; }
 
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + Name + "Age: " + Age);
+
+        }
     }
 }
--- EVEDRI Github Exercise/Program.cs
+++ EVEDRI Github Exercise/Program.cs
@@ -10,6 +10,8 @@ namespace EVEDRI_Github_Exercise
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
